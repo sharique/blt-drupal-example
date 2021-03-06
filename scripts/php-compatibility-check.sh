eec0318aa252a9ac0d058d28b13643c1657531c9
Sharique Ahmed Farooqui <sharique.farooqui@acquia.com>
2021-03-06 17:03:18 +0530

    Updated script.

--- scripts/php-compatibility-check.sh
+++ scripts/php-compatibility-check.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+echo "Running compatibility is check for core."
+./vendor/bin/phpcs  --standard=PHPCompatibility --runtime-set testVersion 7.4 --report-full=./report/core.log --extensions=php,inc,module docroot/core
+
 echo "Running compatibility is check for themes."
 ./vendor/bin/phpcs  --standard=PHPCompatibility --runtime-set testVersion 7.4 --report-full=./report/themes.log --extensions=php,inc,theme docroot/themes
 
 echo "Running compatibility is check for modules."
 ./vendor/bin/phpcs  --standard=PHPCompatibility --runtime-set testVersion 7.4 --report-full=./report/modules.log --extensions=php,inc,module docroot/modules
+
+echo "Running compatibility is check for profiles."
+./vendor/bin/phpcs  --standard=PHPCompatibility --runtime-set testVersion 7.4 --report-full=./report/profiles.log --extensions=php,inc,module docroot/profiles
+
+echo "Running compatibility is check for vendor."
+./vendor/bin/phpcs  --standard=PHPCompatibility --runtime-set testVersion 7.4 --report-full=./report/vendor.log --extensions=php,inc,module vendor
